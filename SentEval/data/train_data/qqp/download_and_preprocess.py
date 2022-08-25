27547932c060ccf6493944f473667fbaa352efb4
Adam Dziedzic <adam.cajf@gmail.com>
2022-08-25 14:16:18 -0400

    read data with pandas

--- SentEval/data/train_data/qqp/download_and_preprocess.py
+++ SentEval/data/train_data/qqp/download_and_preprocess.py
@@ -5,13 +5,18 @@ This file downloads the qqp dataset and preprocesses it into a CSV-file consisti
 Each column contains one question.
 """
 
-qqp_train = load_dataset('merve/qqp' ,split='train', download_mode='force_redownload', cache_dir=".")
-#qqp_test = load_dataset('merve/qqp' ,split='test')
+qqp_train = load_dataset('merve/qqp',
+                         split='train',
+                         # download_mode='force_redownload',
+                         download_mode='reuse_cache_if_exists',
+                         cache_dir=".")
+# qqp_test = load_dataset('merve/qqp' ,split='test')
 
 
 # remove unnecessary columns
-df_qqp_train_q1q2 = qqp_train.remove_columns(["id", "qid1", "qid2", "is_duplicate"])
-#df_qqp_test_q1q2 = qqp_test.remove_columns(["id", "qid1", "qid2", "is_duplicate"])
+df_qqp_train_q1q2 = qqp_train.remove_columns(
+    ["id", "qid1", "qid2", "is_duplicate"])
+# df_qqp_test_q1q2 = qqp_test.remove_columns(["id", "qid1", "qid2", "is_duplicate"])
 
 
 print("a")
---
+++ SentEval/data/train_data/qqp/load_data.py
@@ -0,0 +1,26 @@
+import csv
+import os
+
+import pandas as pd
+
+cwd = os.getcwd()
+print('cwd: ', cwd)
+
+test_data = pd.read_csv(
+    './downloads/148fe59951311f5507e4d3f6ee80a0e392cc736800eb8a06baa3bfe3bc81d8de',
+    sep=',', quoting=csv.QUOTE_ALL)
+print('test data: ', test_data)
+
+test_data_export = test_data[['question1', 'question2']]
+test_data_export.to_csv('qqp_test.csv', index=False, quoting=csv.QUOTE_ALL)
+print('exported test')
+
+train_data = pd.read_csv(
+    './downloads/480158dbb37ad1b381203a9fab5ea0859d072ed4a729712dcc05f52ec73a3136',
+    sep=',', quoting=csv.QUOTE_ALL)
+print('train data: ', train_data)
+
+train_data_export = train_data[['question1', 'question2']]
+train_data_export.to_csv('qqp_train.csv', index=False, quoting=csv.QUOTE_ALL)
+
+print('exported train')
